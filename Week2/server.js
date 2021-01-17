842c8a47d4e220d1f1263ea3fd4b5c9a6664372f
Peter Torres <torres7p@gmail.com>
2021-01-16 22:19:07 -0800

    Initial http servert attempt

---
+++ Week2/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+http.get('http://graph.microsoft.com/v1.0/users', (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    response.on('end', () => {
+        console.log(data);
+    });
+})
+
+.on('error', (error) => {
+    console.log(error);
+})
+
+
+
+const requestListener = function (req, res) {
+    
+    req.on('data', chunk => {
+        console.log(`Data chunk: ${chunk}`)
+    })
+
+    res.writeHead(200);
+    res.end('Hello, World!');
+}
+
+
+
+const server = http.createServer(requestListener);
+server.listen(7777);
